a8942abf8350a2b5de05d7ad8ff255d7b0aa52ba
Rickard Andersson <gonz@severnatazvezda.com>
2015-11-18 18:13:59 +0200

    Add search() and movie_info() for searching for movie data

--- lib/omdb_api.ex
+++ lib/omdb_api.ex
@@ -1,2 +1,30 @@
 defmodule OMDBApi do
+  use HTTPoison.Base
+
+  def process_url(url) do
+    "http://omdbapi.com/" <> url
+  end
+
+  def process_response_body(body) do
+    body
+     |> Poison.decode!
+     |> Enum.map( fn({k,v}) -> {String.to_atom(k), v} end)
+  end
+
+  def search(title) do
+    url = "?s=#{URI.encode_www_form(title)}"
+    get!(url).body[:Search] |> List.first
+  end
+  def search(title, year) do
+    url = "?s=#{URI.encode_www_form(title)}&y=#{year}"
+    get!(url).body[:Search] |> List.first
+  end
+
+  def movie_info(title) do
+    %{"Title" => normalized_title, "Year" => year,
+      "Poster" => poster, "imdbID" => imdb_id} = search(title)
+    url = "?t=#{URI.encode_www_form(normalized_title)}&y=#{year}&plot=short"
+    data = get!(url).body
+    IO.puts data[:Director]
+  end
 end
